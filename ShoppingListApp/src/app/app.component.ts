ce46b9f30df8a2cb6071877a744baa8dfa1582a4
rotkonen <niklas.rotkonen@hotmail.com>
2024-08-05 11:07:45 +0300

    Made so that the log out button is visible when logged in

--- ShoppingListApp/src/app/app.component.ts
+++ ShoppingListApp/src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet,Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet,Router,NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  showLogoutButton: boolean = false;
+
   constructor(private userService: UserService, private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.checkRoute();
+    });
+    this.checkRoute();
+  }
+
+  checkRoute() {
+    const currentRoute = this.router.url;
+    this.showLogoutButton = currentRoute === '/shopping-list';
+  }
 
   onLogout(): void {
     this.userService.logout();
